fc2dabf9e4ddf7dc624225b5336f61a6563c50e5
csamarajev12 <98739412+csamarajev12@users.noreply.github.com>
2022-02-03 09:59:23 +0200

    Add files via upload

---
+++ yl4.py
@@ -0,0 +1,32 @@
+# harjutus 4
+# caroline samarajev
+# 03.03.2022
+
+#juubel
+ 
+
+#matemaatika
+arv1 = input("")
+arv2 = input("")
+tehe = input("vali tehe:\n 1) liitmine\n 2) lahutamine jne")
+
+if tehe==1:
+    teen seda
+elif tehe==2:
+    teen seda
+elif tehe==3:
+    teen seda
+else:
+    teen seda
+
+
+
+
+#Ruudu kontroll
+a = int(input("Sisesta ruudu üks külg: "))
+b = int(input("sisesta ruudu teine külg: "))
+if a == b:
+    print("Tegemist on ruuduga")
+
+else:
+    print("Risttahukas")
